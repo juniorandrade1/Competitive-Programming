0fb5e29d4e2a914ab00be51027ff24cb2e52701a
Junior <junior.andrade.cco@gmail.com>
2017-03-23 12:42:47 -0300

    SWERC Codes

--- Codeforces/SWERC2014/E.cpp
+++ Codeforces/SWERC2014/E.cpp
@@ -29,87 +29,127 @@ typedef vector< ii > vii;
 int dx[] = {-1, 1, 0, 0};
 int dy[] = {0, 0, -1, 1};
 
+const int N = 50;
+
 int r, n, m, maximum;
+char mapa[N][N];
 
 struct field {
-  vector< string > v;
+  ii pos1;
+  vii pos2;
   field();
-  field(vector< string > other) {
-    v = other;
+  field(ii _pos1, vii _pos2) {
+    pos1 = _pos1;
+    pos2 = _pos2;
   }
   bool operator < (field other) const {
-    return v < other.v;
+    if(pos1 != other.pos1) return pos1 < other.pos1;
+    return pos2 < other.pos2;
   }
 };
 
 map< field, int > dis;
 
 bool finishField(field f) {
-  for(int i = 0; i < n; ++i) {
-    for(int j = 0; j < m; ++j) {
-      if(f.v[i][j] == 'X') return false;
-    }
-  }
-  return true;
-}
-
-inline void printField(field foo) {
-  for(int i = 0; i < n; ++i) cout << foo.v[i] << endl;
-  cout << endl;
+  return mapa[f.pos1.F][f.pos1.S] == 'X';
 }
 
 int bfs(field start) {
   queue< field > q; q.push(start); dis[start] = 0;
   while(!q.empty()) {
     field foo = q.front(); q.pop();
-    field cp = foo;
-    if(finishField(foo)) {
-      return dis[foo];
-    }
-    if(dis[foo] == maximum) break;
+    if(finishField(foo)) return dis[foo];
+    if(dis[foo] == maximum) continue;
     int wwwwwww = dis[foo];
-    for(int x = 0; x < n; ++x) {
-      for(int y = 0; y < m; ++y) {
-        if(foo.v[x][y] == '.' || foo.v[x][y] == 'W' || foo.v[x][y] == 'X') continue;
-        for(int k = 0; k < 4; ++k) {
-          int z = 1;
-          while(1) {
-            int xx = x + dx[k] * z, yy = y + dy[k] * z;
-            if(xx < 0 || yy < 0 || xx >= n || yy >= m) break;
-            if(foo.v[xx][yy] == '.' || foo.v[xx][yy] == 'X') {
-              z++;
-            }
-            else break;
+    for(int i = 0; i < foo.pos2.size(); ++i) {
+      int x = foo.pos2[i].F;
+      int y = foo.pos2[i].S;
+      for(int k = 0; k < 4; ++k) {
+        int z = 1;
+        while(1) {
+          int xx = x + dx[k] * z, yy = y + dy[k] * z;
+          if(xx < 0 || yy < 0 || xx >= n || yy >= m) break;
+          if(ii(xx, yy) == foo.pos1) break;
+          int ok = 0;
+          for(int j = 0; j < foo.pos2.size() && !ok; ++j) if(ii(xx, yy) == foo.pos2[j]) ok = 1;
+          if(ok) break;
+          if(mapa[xx][yy] == '.' || mapa[xx][yy] == 'X') {
+            z++;
           }
-          if(z == 1) continue;
-          int xx = x  + dx[k] * (z - 1), yy = y + dy[k] * (z - 1);
-          char pxy = foo.v[x][y], pxxyy = foo.v[xx][yy];
-          if(pxxyy == 'X' && (pxy - '0') >= 2 && (pxy - '0') <= 4) continue;
-          foo.v[xx][yy] = foo.v[x][y];
-          foo.v[x][y] = '.';
-          if(dis.find(foo) == dis.end()) {
-            dis[foo] = wwwwwww + 1;
-            q.push(foo);
-          }
-          foo.v[x][y] = pxy;
-          foo.v[xx][yy] = pxxyy;
+          else break;
+        }
+        if(z == 1) continue;
+        int xx = x  + dx[k] * (z - 1), yy = y + dy[k] * (z - 1);
+        field cp = foo;
+        cp.pos2[i] = ii(xx, yy);
+        sort(cp.pos2.begin(), cp.pos2.end());
+        if(dis.find(cp) == dis.end()) {
+          dis[cp] = dis[foo] + 1;
+          q.push(cp);
         }
       }
     }
-    //exit(0);
+
+
+    int x = foo.pos1.F;
+    int y = foo.pos1.S;
+    for(int k = 0; k < 4; ++k) {
+      int z = 1;
+      while(1) {
+        int xx = x + dx[k] * z, yy = y + dy[k] * z;
+        if(xx < 0 || yy < 0 || xx >= n || yy >= m) break;
+        if(ii(xx, yy) == foo.pos1) break;
+        int ok = 0;
+        for(int j = 0; j < foo.pos2.size() && !ok; ++j) if(ii(xx, yy) == foo.pos2[j]) ok = 1;
+        if(ok) break;
+        if(mapa[xx][yy] == '.' || mapa[xx][yy] == 'X') {
+          z++;
+        }
+        else break;
+      }
+      if(z == 1) continue;
+      int xx = x  + dx[k] * (z - 1), yy = y + dy[k] * (z - 1);
+      field cp = foo;
+      cp.pos1 = ii(xx, yy);
+      if(dis.find(cp) == dis.end()) {
+        dis[cp] = dis[foo] + 1;
+        q.push(cp);
+      }
+    }
   }
   return -1;
 }
 
 int main() {
   scanf("%d %d %d %d", &r, &m, &n, &maximum);
-  vector< string > f;
+  ii pos1;
+  vii pos2;
   for(int i = 0; i < n; ++i) {
-    char s[123]; scanf(" %s", s);
-    for(int j = 0; j < m; ++j) if(s[j] >= '2' && s[j] <= '5') s[j] = '2';
-    f.pb((string)s);
+    scanf(" %s", mapa[i]);
+    for(int j = 0; j < m; ++j) {
+      if(mapa[i][j] >= '2' && mapa[i][j] <= '5') mapa[i][j] = '2';
+      if(mapa[i][j] == '1') {
+        pos1 = ii(i, j);
+        mapa[i][j] = '.';
+      }
+      if(mapa[i][j] == '2') {
+        pos2.pb(mp(i, j));
+        mapa[i][j] = '.';
+      }
+    }
   }
-  int d = bfs(field(f));
+  sort(pos2.begin(), pos2.end());
+
+  /*
+  for(int i = 0; i < n; ++i) {
+    for(int j = 0; j < m; ++j) {
+      debug("%c", mapa[i][j]);
+    }
+    debug("\n");
+  }
+  */
+
+  int d = bfs(field(pos1, pos2));
   if(d == -1) printf("NO SOLUTION\n");
   else printf("%d\n", d);
   return 0;
